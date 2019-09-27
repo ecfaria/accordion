ccd95df783d9c6fbb3c38af7d895de264058a8ef
Emilio <emiliocfaria@gmail.com>
2019-09-27 13:56:14 +0100

    animated transition + aria

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,40 @@
-const openedClass = 'opened';
 const accordions = document.querySelectorAll('.accordion');
 
 accordions.forEach(accordion => {
   const items = accordion.querySelectorAll('.accordion__item');
 
   items.forEach(item => {
-    const header = item.getElementsByClassName('accordion__header');
-    const body = item.getElementsByClassName('accordion__body');
-    header[0].addEventListener('click', () => {
-      toggleAccordion(item, items);
+    const header = item.getElementsByClassName('accordion__header')[0];
+    header.addEventListener('click', () => {
+      toggleAccordion(item, items, header);
     });
   });
 });
 
-const toggleAccordion = (el, items) => {
-  if (el.classList.contains(openedClass)) {
-    el.classList.remove(openedClass);
+const toggleAccordion = (el, items, header) => {
+  if (el.classList.contains('opened')) {
+    el.classList.remove('opened');
+    el.getElementsByClassName('accordion__tab')[0].setAttribute(
+      'aria-hidden',
+      true
+    );
+    header.setAttribute('aria-expanded', false);
   } else {
     closeAccordion(items);
-    el.classList.add(openedClass);
+    el.classList.add('opened');
+    el.getElementsByClassName('accordion__tab')[0].setAttribute(
+      'aria-hidden',
+      false
+    );
+    header.setAttribute('aria-expanded', true);
   }
 };
 
 const closeAccordion = el => {
   for (let item of el) {
-    item.classList.remove(openedClass);
+    item.classList.remove('opened');
+    item
+      .getElementsByClassName('accordion__header')[0]
+      .setAttribute('aria-expanded', false);
   }
 };
